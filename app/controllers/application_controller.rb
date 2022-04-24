6f167cf89628452419f32090036027b770458a6b
Drexal72 <alexandre.ortuno@tutanota.com>
2022-04-24 19:58:34 +0200

    alex signup in progress + db user update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
+    
+        
+     
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:account_update, keys: [:name, :first_name, :desc, :price, :lang, :nationality])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:userName, :firstName, :lastName,])
     end    
 end
